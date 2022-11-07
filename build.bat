4d2771ec1a796b6989029ce9ef866617efa7d957
shainsahshs <117613288+shainsahshs@users.noreply.github.com>
2022-11-07 19:24:31 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-pyinstaller --icon="Resources/cbIcon.ico" --onefile --log-level ERROR interpreter.py
-move .\dist\interpreter.exe .\
-@RD /S /Q .\dist
-@RD /S /Q .\build
-del cbLang.exe
-del interpreter.spec
-rename interpreter.exe cbLang.exe
